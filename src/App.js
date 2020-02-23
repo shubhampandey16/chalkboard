13232bac193e97d156e6e8041fe0b9a4349ad27c
Shubham Pandey <shubham.in.pandey@gmail.com>
2020-02-23 14:54:08 -0800

    skeleton done

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Chalkboard
+        </p>
+        <a
+          className="App-link"
+          href=""
+          target="_blank"
+        >
+          Chalkboard
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
